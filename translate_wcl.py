e744bce1ecff7b9e2871dbee882d9d317e642f43
yiping <yiping@yiping-LIFEBOOK-LH531.(none)>
2013-10-15 16:54:08 +0800

    finish first step

--- translate_wcl.py
+++ translate_wcl.py
@@ -1,85 +1,110 @@
 # translate_wcl.py
 # parse wcl corpus, prepare input to our system
-# Yiping 2012
+# @author: Yiping 2013
 
 import re
 import random
+
+def prepare_columns():
+    """ calls parse_wcl and extract the input files for positive and negative input
+    """
+    #Parameters
+    good_path    = './wiki_good.txt' #location of the documents
+    bad_path = './wiki_bad.txt'
+
+    good_output = './wiki_good.column'
+    bad_output = './wiki_bad.column'
+
+    process_wcl(good_path, good_output, True)  #parse wcl format at good_path and output to good_output
+    process_wcl(bad_path, bad_output, False)  #parse wcl format at bad_path and output to bad_output
+  
+# End of prepare_columns()
+
+def process_wcl(input_path, output_path, is_definition_sentence):
+    """ parse the wcl corpus from input_path and write as column feature format
+
+    the output are in wiki_good.column and wiki_bad.column
+    each line contains a single word, columns are separated 
+    by a tab. Columns represent (sentence label, token label,
+    word, pos, chunking tab)
+
+    Args:
+        input_path: file path to the input file, original wcl corpus
+        output_path: file path to the output file
+        is_definition_sentence: boolean if the file contains definition or non-definition sentences
+    """
+    f = open(input_path,'r')
+    sentences = f.readlines()
+    f.close()
+    
+    f = open(output_path,"w+")
+   
+    for sentence in sentences:
+	sentence = sentence.strip()
+	if len(sentence)>1 and not sentence.startswith("#"):
+	    new_sent = ""
+	    startDef = False
+	    tokens = sentence.split("\t")
+
+            # locate the term
+            target = tokens[0]
+	    term = target[:target.find(":")]
+
+            chunk_feature = "NULL"
+            pos_feature = "NULL"
+            word_feature = "NULL"
+            # fixed sentence label
+            if is_definition_sentence:
+                sentence_label = "1"
+            else:
+                sentence_label = "0"
+            word_label = "O"
+
+	    for token in tokens:
+		if '_' in token:
+                    columns = token.split('_')
+                    if(len(columns)==3):  #it's a word
+		        chunk_feature = columns[0]
+                        pos_feature = columns[1]
+                        word_feature = columns[2]
+                        if startDef:  # the word is a hypernym
+                            word_label = "HYPER"
+                            startDef = False
+                        elif word_feature == "TARGET":  # the word is a term
+                            word_feature = term
+                            word_label = "TERM"
+                        else:
+                            word_label = "O"
+                    elif(len(columns)==2):  #it's a punctuation mark   
+		        chunk_feature = "NULL"
+                        pos_feature = columns[0]
+                        word_feature = columns[1]
+                        word_label = "O"
+                    #add a new line to output file
+                    f.write("%s\n" % "\t".join([sentence_label, word_label, word_feature, pos_feature, chunk_feature])) 
+
+		if token == "<HYPER>":
+		    startDef = True
+            
+            f.write("\n")  # put an empty line between each sentence 	        
+                
+	else:
+	    pass#f.write(sentence)
+    f.close() 
+# end of process_wcl
+
+def extract_features():
+    """ extract the features from the .column input file
+
+    """
+# End of extract_features()
+
 ################################################
 #                 Main Function                #
 ################################################
 
 
 if __name__ == '__main__' : 
-    
-    #Parameters
-    good_path    = './wikipedia/wiki_good.txt' #location of the documents
-    bad_path = './wikipedia/wiki_bad.txt'
-    #open file for reading
-    f = open(good_path,'r')
-    #read all the lines and store them in a list
-    sentences = f.readlines()
-    #file no longer needs to be open
-    f.close()
-    
-    f = open("./wiki_good_yp.txt","w+")
-    print(len(sentences))
-    for sentence in sentences:
-	sentence = sentence.strip()
-	if len(sentence)>1 and not sentence.startswith("#"):
-	    new_sent = ""
-	    startDef = False
-	    tokens = sentence.split("\t")
-	    target = tokens[0]
-	    target = target[:target.find(":")]
-	    for token in tokens:
-		if '_' in token:
-		    word = token[token.rfind('_')+1:]
-		    if not startDef:
-		        new_sent += word + " "
-		    else:
-			new_sent += word + "/DEF "
-			startDef = False
-		if token == "<GENUS>":
-		    startDef = True
-	    #f.write("\n".join(tokens))
-	    new_sent = new_sent.replace("TARGET", target+"/TERM")
-	    new_sent = new_sent[:-1] + "/O"
-	    f.write(new_sent+"\n")
-	else:
-	    pass#f.write(sentence)
-    f.close()  
 
-    #open file for reading
-    f = open(bad_path,'r')
-    #read all the lines and store them in a list
-    sentences = f.readlines()
-    #file no longer needs to be open
-    f.close()
-    
-    f = open("./wiki_bad_yp.txt","w+")
-    print(len(sentences))
-    for sentence in sentences:
-	sentence = sentence.strip()
-	if len(sentence)>1 and not sentence.startswith("#"):
-	    new_sent = ""
-	    startDef = False
-	    tokens = sentence.split("\t")
-	    target = tokens[0]
-	    target = target[1:target.find(":")]
-	    for token in tokens:
-		if '_' in token:
-		    word = token[token.rfind('_')+1:]
-		    if not startDef:
-		        new_sent += word + " "
-		    else:
-			new_sent += word + "/DEF "
-			startDef = False
-		if token == "<GENUS>":
-		    startDef = True
-	    #f.write("\n".join(tokens))
-	    new_sent = new_sent.replace("TARGET", target)
-	    new_sent = new_sent[:-1]
-	    f.write(new_sent+"\n")
-	else:
-	    pass#f.write(sentence)
-    f.close()  
+    prepare_columns()
+    extract_features()
