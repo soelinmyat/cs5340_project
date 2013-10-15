7971257706c81bbe3f12ee8091aabd0dd807efce
yiping <yiping@yiping-LIFEBOOK-LH531.(none)>
2013-10-15 17:23:53 +0800

    finish with toy example

--- translate_wcl.py
+++ translate_wcl.py
@@ -5,18 +5,24 @@
 import re
 import random
 
-def prepare_columns():
+def main():
     """ calls parse_wcl and extract the input files for positive and negative input
     """
-    #Parameters
+   
     good_path    = './wiki_good.txt' #location of the documents
     bad_path = './wiki_bad.txt'
 
-    good_output = './wiki_good.column'
-    bad_output = './wiki_bad.column'
+    good_intermediate = './wiki_good.column'
+    bad_intermediate = './wiki_bad.column'
 
-    process_wcl(good_path, good_output, True)  #parse wcl format at good_path and output to good_output
-    process_wcl(bad_path, bad_output, False)  #parse wcl format at bad_path and output to bad_output
+    good_output = './wiki_good.output'
+    bad_output = './wiki_bad.output'
+
+    process_wcl(good_path, good_intermediate, True)  #parse wcl format at good_path and output to good_output
+    process_wcl(bad_path, bad_intermediate, False)  #parse wcl format at bad_path and output to bad_output
+
+    extract_features(good_intermediate, good_output)  #from the column representation prepare input file to GRMM
+    extract_features(bad_intermediate, bad_output)  #from the column representation prepare input file to GRMM
   
 # End of prepare_columns()
 
@@ -93,10 +99,38 @@ def process_wcl(input_path, output_path, is_definition_sentence):
     f.close() 
 # end of process_wcl
 
-def extract_features():
+def extract_features(input_path, output_path):
     """ extract the features from the .column input file
 
+    the input are in wiki_good.column and wiki_bad.column
+    each line contains a single word, columns are separated 
+    by a tab. Columns represent (sentence label, token label,
+    word, pos, chunking tab)
+    The output file follows grmm requirement
+
+    Args:
+        input_path: file path to the input file, *.column
+        output_path: file path to the output file
     """
+    f = open(input_path,'r')
+    words = f.readlines()
+    f.close()
+
+    f = open(output_path,"w+")
+    
+    for word in words:
+        if not "\t" in word:  # empty line
+            f.write("\n")
+        else:
+            columns = word.strip().split("\t")
+            chunk_feature = columns[4]
+            pos_feature = columns[3]
+            word_feature = columns[2]
+            word_label = columns[1]
+            sentence_label = columns[0]
+         
+            f.write("%s %s ---- Word=%s Pos=%s Chunk=%s\n" % (sentence_label, word_label, word_feature, pos_feature, chunk_feature)) 
+    
 # End of extract_features()
 
 ################################################
@@ -106,5 +140,5 @@ def extract_features():
 
 if __name__ == '__main__' : 
 
-    prepare_columns()
-    extract_features()
+    main()
+    
