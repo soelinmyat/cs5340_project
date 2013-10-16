ed0ee3428f3c03a36859dca7d7f1e86a3e75389d
Soe Lin Myat <soelinmyat@gmail.com>
2013-10-17 04:40:09 +0800

    added extract_extended_features function and updated split_test_and_training_data

--- translate_wcl.py
+++ translate_wcl.py
@@ -25,10 +25,13 @@ def main():
   process_wcl(good_path, good_intermediate, True)  #parse wcl format at good_path and output to good_output
   process_wcl(bad_path, bad_intermediate, False)  #parse wcl format at bad_path and output to bad_output
 
-  extract_features(good_intermediate, good_output)  #from the column representation prepare input file to GRMM
-  extract_features(bad_intermediate, bad_output)  #from the column representation prepare input file to GRMM
+  extract_extended_features(good_intermediate, good_output)
+  extract_extended_features(bad_intermediate, bad_output)
 
-  split_test_and_training_data(good_output, bad_output, training_txt, test_txt, 0.9)
+  #extract_features(good_intermediate, good_output)  #from the column representation prepare input file to GRMM
+  #extract_features(bad_intermediate, bad_output)  #from the column representation prepare input file to GRMM
+
+  split_test_and_training_data(good_output, bad_output, training_txt, test_txt, 3000, 3000)
   
 # End of prepare_columns()
 
@@ -45,11 +48,11 @@ def process_wcl(input_path, output_path, is_definition_sentence):
     output_path: file path to the output file
     is_definition_sentence: boolean if the file contains definition or non-definition sentences
   """
-  f = open(input_path,'r')
+  f = open(input_path,'r', encoding="utf8")
   sentences = f.readlines()
   f.close()
     
-  f = open(output_path,"w+")
+  f = open(output_path,"w+", encoding="utf8")
    
   for sentence in sentences:
     sentence = sentence.strip()
@@ -62,7 +65,7 @@ def process_wcl(input_path, output_path, is_definition_sentence):
       target = tokens[0]
       term = ""
       if is_definition_sentence:
-	    term = target[:target.find(":")]
+	      term = target[:target.find(":")]
       else:
         term = target[1:target.find(":")]
       chunk_feature = "NULL"
@@ -125,11 +128,11 @@ def extract_features(input_path, output_path):
     input_path: file path to the input file, *.column
     output_path: file path to the output file
   """
-  f = open(input_path,'r')
+  f = open(input_path,'r', encoding="utf8")
   words = f.readlines()
   f.close()
 
-  f = open(output_path,"w+")
+  f = open(output_path,"w+", encoding="utf8")
     
   for word in words:
     if not "\t" in word:  # empty line
@@ -145,7 +148,80 @@ def extract_features(input_path, output_path):
       f.write("%s %s ---- Word=%s Pos=%s Chunk=%s\n" % (sentence_label, word_label, word_feature, pos_feature, chunk_feature))
   f.close()
 
-def split_test_and_training_data(good_input_path, bad_input_path, training_txt_path, test_txt_path, training_to_test_ratio):
+def extract_extended_features(input_path, output_path):
+  """ extract the features from the .column input file
+
+  The difference between this function and extract_features function is that
+  this function includes words from the same sentence and their features in the
+  feature set of each word.
+
+  the input are in wiki_good.column and wiki_bad.column
+  each line contains a single word, columns are separated 
+  by a tab. Columns represent (sentence label, token label,
+  word, pos, chunking tab)
+  The output file follows grmm requirement
+
+  Args:
+    input_path: file path to the input file, *.column
+    output_path: file path to the output file
+  """
+  f = open(input_path,'r', encoding="utf8")
+  words = f.readlines()
+  f.close()
+    
+  lines = []
+  words_in_a_line = []
+  for word in words:
+    if not "\t" in word:  # empty line
+      lines.append(words_in_a_line)
+      words_in_a_line = []
+    else:
+      words_in_a_line.append(word)
+
+  f = open(output_path,"w+", encoding="utf8")
+
+  for line in lines:
+    num_words = len(line)
+    avg_word_length = int(sum(len(word.strip().split("\t")[2]) for word in line)/num_words)
+
+    for word in line:
+      current_index = line.index(word)
+      columns = word.strip().split("\t")
+      word_label = columns[1]
+      sentence_label = columns[0]
+      features = ""
+      for num in range(0,num_words-1):
+        features += extract_features_from_row(line[num], num - current_index)
+        if num != num_words-1:
+          features += " "
+
+      f.write("%s %s ---- NumWords=%i AvgWordsLength=%i %s\n" % (sentence_label, word_label, num_words, avg_word_length, features))
+
+  f.close()
+
+def extract_features_from_row(word, position):
+  """ extract the features from a row from .column file
+
+  Args:
+    word: a row in the .column file
+    position: position of the current word in the sentence
+  """
+  columns = word.strip().split("\t")
+  chunk_feature = columns[4]
+  pos_feature = columns[3]
+  word_feature = columns[2]
+  word_length = len(columns[2])
+
+  features = ""
+  if position == 0:
+    features = "Word={} Pos={} Chunk={} Length={}".format(word_feature, pos_feature, chunk_feature, word_length)
+  else:
+    features = "Word={1}@{0} Pos={2}@{0} Chunk={3}@{0} Length={4}@{0}".format(position, word_feature, pos_feature, chunk_feature, word_length)
+
+  return features
+
+
+def split_test_and_training_data(good_input_path, bad_input_path, training_txt_path, test_txt_path, training_size, test_size):
   """ get .output files and split them into training.txt and test.txt based on training_to_test_ratio
 
   the input are in wiki_good.out and wiki_bad.out
@@ -156,28 +232,56 @@ def split_test_and_training_data(good_input_path, bad_input_path, training_txt_p
     bad_input_path: file path to the bad output file (e.g. wiki_bad.output)
     training_txt_path: file path to the training output file
     test_txt_pat: file path to the test output file
-    training_to_test_ratio: the ratio of training to test data
+    training_size: the maximum size of training dataset
+    test_size: the maximum size of test dataset
   """
-  f = open(good_input_path)
+  training_num_terms = int(training_size * 0.2)
+  training_num_hyper = int(training_size * 0.2)
+  training_num_others = training_size - training_num_terms - training_num_hyper
+  test_num_terms = int(test_size * 0.2)
+  test_num_hyper = int(training_size * 0.2)
+  test_num_others = test_size - test_num_terms - training_num_hyper
+
+  f = open(good_input_path, encoding="utf8")
   lines = f.readlines();
   f.close()
 
-  f = open(bad_input_path)
+  f = open(bad_input_path, encoding="utf8")
   lines += (f.readlines())
   f.close()
 
   shuffle(lines)
-  num_training_lines = int(len(lines) * training_to_test_ratio)
 
-  count = 0
-  f1 = open(training_txt_path, "w+")
-  f2 = open(test_txt_path, "w+")
+  f1 = open(training_txt_path, "w+", encoding="utf8")
+  f2 = open(test_txt_path, "w+", encoding="utf8")
   for line in lines:
-    if count < num_training_lines:
-      f1.write("%s" % line)
+    if "TERM" in line:
+      if training_num_terms > 0:
+        f1.write("%s" % line)
+        training_num_terms = training_num_terms - 1
+      elif test_num_terms > 0:
+        f2.write("%s" % line)
+        test_num_terms = test_num_terms - 1
+      else:
+        pass 
+    elif "HYPER" in line:
+      if training_num_hyper > 0:
+        f1.write("%s" % line)
+        training_num_hyper = training_num_hyper - 1
+      elif test_num_hyper > 0:
+        f2.write("%s" % line)
+        test_num_hyper = test_num_hyper - 1
+      else:
+        pass 
     else:
-      f2.write("%s" % line)
-    count = count + 1
+      if training_num_others > 0:
+        f1.write("%s" % line)
+        training_num_others = training_num_others - 1
+      elif test_num_others > 0:
+        f2.write("%s" % line)
+        test_num_others = test_num_others - 1
+      else:
+        pass 
 
   f1.close()
   f2.close()
@@ -189,8 +293,6 @@ def split_test_and_training_data(good_input_path, bad_input_path, training_txt_p
 #                 Main Function                #
 ################################################
 
-
-if __name__ == '__main__' : 
-
+if __name__ == '__main__' :
     main()
     
