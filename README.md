530ec99e2ff60d90b00f98652bf3124c64e6d02b
SoeLinMyat <soelinmyat@soelinmyat-OptiPlex-960.(none)>
2013-10-16 18:19:01 +0800

    implemented split_test_and_training_data function. changed labels - e.g. 0 to Other for terms and 1 to DS and 0 to NDS for sentence

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 cs5340_project
 ==============
+
+
+Temp note
+
+java -Xms2000m -cp $GRMM/class:$GRMM/lib/mallet-deps.jar:$GRMM/lib/grmm-deps.jar edu.umass.cs.mallet.grmm.learning.GenericAcrfTui --training $GRMM/data/toy/training.txt --testing $GRMM/data/toy/test.txt --model-file tmpls.txt > stdout.txt 2> stderr.txt
--- translate_wcl.py
+++ translate_wcl.py
@@ -4,6 +4,7 @@
 
 import re
 import random
+from random import shuffle
 
 def main():
   """ calls parse_wcl and extract the input files for positive and negative input
@@ -18,16 +19,22 @@ def main():
   good_output = './wiki_good.output'
   bad_output = './wiki_bad.output'
 
+  training_txt = './training.txt'
+  test_txt = './test.txt'
+
   process_wcl(good_path, good_intermediate, True)  #parse wcl format at good_path and output to good_output
   process_wcl(bad_path, bad_intermediate, False)  #parse wcl format at bad_path and output to bad_output
 
   extract_features(good_intermediate, good_output)  #from the column representation prepare input file to GRMM
   extract_features(bad_intermediate, bad_output)  #from the column representation prepare input file to GRMM
+
+  split_test_and_training_data(good_output, bad_output, training_txt, test_txt, 0.9)
   
 # End of prepare_columns()
 
 def process_wcl(input_path, output_path, is_definition_sentence):
   """ parse the wcl corpus from input_path and write as column feature format
+
   the output are in wiki_good.column and wiki_bad.column
   each line contains a single word, columns are separated 
   by a tab. Columns represent (sentence label, token label,
@@ -63,38 +70,40 @@ def process_wcl(input_path, output_path, is_definition_sentence):
       word_feature = "NULL"
       # fixed sentence label
       if is_definition_sentence:
-        sentence_label = "1"
+        sentence_label = "DS"
       else:
-        sentence_label = "0"
-        word_label = "O"
+        sentence_label = "NDS"
+      word_label = "OTHER"
 
-        for token in tokens:
-            if '_' in token:
-              if ":" in token:
-                token = token[target.find(":")+1:]
-              columns = token.split('_')
-              if(len(columns)==3):  #it's a word
-                chunk_feature = columns[0]
-                pos_feature = columns[1]
-                word_feature = columns[2]
-                if startDef:  # the word is a hypernym
-                  word_label = "HYPER"
-                  startDef = False
-                elif word_feature == "TARGET":  # the word is a term
-                  word_feature = term
-                  word_label = "TERM"
-                else:
-                  word_label = "O"
-              elif(len(columns)==2):  #it's a punctuation mark   
-                chunk_feature = "NULL"
-                pos_feature = columns[0]
-                word_feature = columns[1]
-                word_label = "O"
-                
-              #add a new line to output file
-              f.write("%s\n" % "\t".join([sentence_label, word_label, word_feature, pos_feature, chunk_feature])) 
-            if token == "<HYPER>":
-              startDef = True
+      for token in tokens:
+        if '_' in token:
+          if ":" in token:
+            token = token[target.find(":")+1:]
+
+          columns = token.split('_')
+          if(len(columns)==3):  #it's a word
+            chunk_feature = columns[0]
+            pos_feature = columns[1]
+            word_feature = columns[2]
+            if startDef:  # the word is a hypernym
+              word_label = "HYPER"
+              startDef = False
+            elif word_feature == "TARGET":  # the word is a term
+              word_feature = term
+              word_label = "TERM"
+            else:
+              word_label = "OTHER"
+          elif(len(columns)==2):  #it's a punctuation mark   
+            chunk_feature = "NULL"
+            pos_feature = columns[0]
+            word_feature = columns[1]
+            word_label = "OTHER"
+        
+          #add a new line to output file
+          f.write("%s\n" % "\t".join([sentence_label, word_label, word_feature, pos_feature, chunk_feature])) 
+        
+        if token == "<HYPER>":
+          startDef = True
             
       f.write("\n")  # put an empty line between each sentence 	                        
     else:
@@ -124,7 +133,7 @@ def extract_features(input_path, output_path):
     
   for word in words:
     if not "\t" in word:  # empty line
-      f.write("\n")
+      pass #f.write("\n")
     else:
       columns = word.strip().split("\t")
       chunk_feature = columns[4]
@@ -133,7 +142,46 @@ def extract_features(input_path, output_path):
       word_label = columns[1]
       sentence_label = columns[0]
          
-      f.write("%s %s ---- Word=%s Pos=%s Chunk=%s\n" % (sentence_label, word_label, word_feature, pos_feature, chunk_feature)) 
+      f.write("%s %s ---- Word=%s Pos=%s Chunk=%s\n" % (sentence_label, word_label, word_feature, pos_feature, chunk_feature))
+  f.close()
+
+def split_test_and_training_data(good_input_path, bad_input_path, training_txt_path, test_txt_path, training_to_test_ratio):
+  """ get .output files and split them into training.txt and test.txt based on training_to_test_ratio
+
+  the input are in wiki_good.out and wiki_bad.out
+  the output file follows grmm requirement
+
+  Args:
+    good_input_path: file path to the good output file (e.g. wiki_good.output)
+    bad_input_path: file path to the bad output file (e.g. wiki_bad.output)
+    training_txt_path: file path to the training output file
+    test_txt_pat: file path to the test output file
+    training_to_test_ratio: the ratio of training to test data
+  """
+  f = open(good_input_path)
+  lines = f.readlines();
+  f.close()
+
+  f = open(bad_input_path)
+  lines += (f.readlines())
+  f.close()
+
+  shuffle(lines)
+  num_training_lines = int(len(lines) * training_to_test_ratio)
+
+  count = 0
+  f1 = open(training_txt_path, "w+")
+  f2 = open(test_txt_path, "w+")
+  for line in lines:
+    if count < num_training_lines:
+      f1.write("%s" % line)
+    else:
+      f2.write("%s" % line)
+    count = count + 1
+
+  f1.close()
+  f2.close()
+
     
 # End of extract_features()
 
